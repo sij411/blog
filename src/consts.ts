58a3990dcff93c1ef04abd48f5c5bc4a18dd3ace
Jiwon Kwon <kjiwon411@gmail.com>
2024-09-13 02:26:19 +0900

    change header and footer

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = "Jiwon Kwon";
+export const SITE_DESCRIPTION = "권지원의 블로그 입니다.";
