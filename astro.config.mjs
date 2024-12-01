063b60427db35b3d16b53ece9849d79e934ef9bf
Jiwon Kwon <kjiwon411@gmail.com>
2024-12-01 15:01:38 +0900

    update site url

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://sij411.github.io",
+  site: "https://sij411.github.io/sij411",
   base: "/",
   integrations: [mdx(), sitemap(), tailwind()],
 });
