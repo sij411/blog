0efeffb8acc9f3b316bb5c2c3da5898b05f6ff2a
Jiwon Kwon <kjiwon411@gmail.com>
2024-12-01 15:20:12 +0900

    test link issue

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://sij411.github.io/sij411",
-  base: "https://sij411.github.io/sij411",
+  site: "https://sij411.github.io/",
+
   integrations: [mdx(), sitemap(), tailwind()],
 });
